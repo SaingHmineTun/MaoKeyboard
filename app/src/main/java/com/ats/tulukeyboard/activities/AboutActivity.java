534324faf061a84623614029db3dd6542d534f77
Mein <saisainghminetun@gmail.com>
2024-08-20 14:03:47 +0630

    RELEASE : v1.1

--- app/src/main/java/com/ats/tulukeyboard/activities/AboutActivity.java
+++ app/src/main/java/com/ats/tulukeyboard/activities/AboutActivity.java
@@ -8,9 +8,11 @@ import com.ats.tulukeyboard.R;
 
 
 public class AboutActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
     }
 }
